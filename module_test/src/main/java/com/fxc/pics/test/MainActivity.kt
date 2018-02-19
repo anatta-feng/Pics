bb93eaf22a9d12c946a935113b74777f5b39e5f5
fxc0719 <1@fxcdev.com>
2018-02-19 23:20:49 +0800

    add test module

---
+++ module_test/src/main/java/com/fxc/pics/test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fxc.pics.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.test_activity_main)
+	}
+}
---
+++ module_test/src/main/java/debug/ModulePicApplication.kt
@@ -0,0 +1,14 @@
+package debug
+
+import android.app.Application
+
+/**
+ *
+ * @author fxc
+ * @date 2018/1/14
+ */
+class ModulePicApplication : Application() {
+	override fun onCreate() {
+		super.onCreate()
+	}
+}
