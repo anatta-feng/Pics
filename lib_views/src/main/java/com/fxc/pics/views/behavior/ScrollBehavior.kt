54ccaaa150c14df617248c3992a86acad6e598fc
fxc0719 <1@fxcdev.com>
2018-03-06 11:13:05 +0800

    修复 Image 滚动不准确的 bug 现存问题：Image 滚出屏幕后还在随着 RecyclerView 在滚动，后面修复（以我的尿性可能不会修复了）

--- lib_views/src/main/java/com/fxc/pics/views/behavior/ScrollBehavior.kt
+++ lib_views/src/main/java/com/fxc/pics/views/behavior/ScrollBehavior.kt
@@ -5,7 +5,6 @@ import android.support.design.widget.CoordinatorLayout
 import android.support.v4.view.ViewCompat
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import kotlin.math.abs
 
@@ -22,27 +21,16 @@ class ScrollBehavior(context: Context, attrs: AttributeSet) : CoordinatorLayout.
 
 	override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: View, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
 		super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type)
-		val rr = target as RecyclerView
-		Log.d("weadasd", "scrollState ${rr.scrollState}")
-		val offset = rr.computeVerticalScrollOffset()
-		val canScrollToDown = target.canScrollVertically(-1)
-//		val canScrollToUp = target.canScrollVertically(1)
-		var delta = dyConsumed
-//		if (canScrollToDown && dyConsumed < 0) {
-//			if (abs(dyConsumed) > offset) {
-//				var temp = 0
-//				if (child.top < 0) {
-//					temp = child.top
-//				}
-//				delta = offset + temp
-//			}
-//			ViewCompat.offsetTopAndBottom(child, (-delta * fraction).toInt())
-//			return
-//		}
-//		if (canScrollToUp && dyConsumed > 0) {
-//			Log.d("qweasd", "canScrollToUp $delta")
-//			ViewCompat.offsetTopAndBottom(child, (-delta * fraction).toInt())
-//		}
+		if (target !is RecyclerView) {
+			return
+		}
+		val offset = target.computeVerticalScrollOffset()
+		if (offset == 0) {
+			if (child.top != 0) {
+				ViewCompat.offsetTopAndBottom(child, -child.top)
+				return
+			}
+		}
 		ViewCompat.offsetTopAndBottom(child, (-dyConsumed * fraction).toInt())
 	}
 
